c279f8d13eefb67bc361576c51e9a996bece7337
Victor Navarro <victor.navarro@bootcamp-upgrade.com>
2022-01-07 17:53:43 +0100

    Iteration#1

---
+++ index.js
@@ -0,0 +1,18 @@
+//Iteration#1
+
+/* Crea una arrow function que tenga dos parametros a y b y 
+que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
+por consola la suma de los dos parametros. */
+
+//1.1 Ejecuta esta función sin pasar ningún parametro
+const sumFunc =  (a = 10, b = 5) => {
+    console.log(a + b);
+}
+
+sumFunc();
+
+//1.2 Ejecuta esta función pasando un solo parametro
+sumFunc(15);
+
+//1.3 Ejecuta esta función pasando dos parametros
+sumFunc(15, 20);
